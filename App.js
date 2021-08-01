5a0de90d9de6ab91f566ca76ff8a7742fed285b9
theluanz <luanzuffo.lz@gmail.com>
2021-07-31 23:14:31 -0300

    light status bar

--- App.js
+++ App.js
@@ -31,7 +31,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar />
+      <StatusBar barStyle="light-content" />
       <Routes />
     </ThemeProvider>
   );
